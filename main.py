eaf5c414835392bb180ac532841030824e84f61c
fehoffcial <feliphe@tuta.io>
2022-12-14 22:51:58 -0300

    Resultado do Exercio da empresa PopConvert

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# PopConvert
 """ Selection process of PopConvert company """
 """
 # Você é dado duas listas ligadas não-vazias representando dois números inteiros não-negativos. Os dígitos são armazenados em ordem reverse, e há somente um dígito em cada posição das listas. Adicione os dois números e retorne a soma como uma lista ligada.
@@ -17,3 +16,12 @@
         # Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
         # Output: [8,9,9,9,0,0,0,1]
 """
+x = int(input("Digite o valor de l1: ")) #* O Valor de l1 para depois fazer a soma com l2
+y = int(input("Digite o valor de l2: ")) 
+xy = x+y #*  A Soma de x e y
+xy = str(xy) #* Convertendo INT para STR para ser utilizado na lista 
+xl = list(xy) #* Criando a LISTA com os VALORES
+xr = list(xy)
+xl.reverse() #! REALIZANDO REVERSE NA LISTA PARA EXIBIR COM SUCESSO
+print("Vamos exibir como que é feito o Processo do Exercio:")
+print("1-) Primeiramente fazemos a soma do valores {0} e {1} que o resultado é: {2} e realizando o reverse: {3}".format(x,y,xr,xl ))
